1dadee46eddd03d7a1dde4306ee5543834d386a6
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-03 15:55:25 +0100

    Added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
