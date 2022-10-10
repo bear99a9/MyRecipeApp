2d85b8e9b6b4951ff72b8bcc3b55df5fa44bf787
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-10 12:08:30 +0100

    Added ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [];
   constructor() { }
 
   ngOnInit(): void {
