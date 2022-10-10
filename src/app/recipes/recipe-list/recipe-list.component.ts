eb5a704438670089a513716362a8743f2a488ceb
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-10 11:58:57 +0100

    Added in recipe detail html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is a test', 
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?resize=960,872?quality=90&webp=true&resize=375,341'),
+    new Recipe('A test recipe', 'this is a test', 
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?resize=960,872?quality=90&webp=true&resize=375,341')
   ];
   constructor() { }
