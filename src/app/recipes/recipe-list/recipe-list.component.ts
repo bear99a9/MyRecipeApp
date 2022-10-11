61b8dc57529bbcf06af81c7d5013b4b9883fc9c4
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-11 16:35:40 +0100

    Removed hard ngFor out into recipe list and added data binding on recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 
+    new Recipe('First test recipe', 'this is the first test recipe', 
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?resize=960,872?quality=90&webp=true&resize=375,341'),
-    new Recipe('A test recipe', 'this is a test', 
+    new Recipe('Second test recipe', 'this is the second test', 
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?resize=960,872?quality=90&webp=true&resize=375,341')
   ];
   constructor() { }
