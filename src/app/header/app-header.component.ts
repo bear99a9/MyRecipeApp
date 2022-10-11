28d566714ef3576dec72afd8ccec129e15597bc6
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-11 16:22:05 +0100

    Quick navigation without routing

--- src/app/header/app-header.component.ts
+++ src/app/header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-header.component.css']
 })
 export class AppHeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
   collapsed = true;
+
+  onSelect(type: string) {
+    this.featureSelected.emit(type);
+  }
+
 }
