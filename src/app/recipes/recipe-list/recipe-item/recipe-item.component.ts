853589aebbdfb57d673ff58135f67cac2a8aad39
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-12 06:46:38 +0100

    Added data binding to select recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,9 +8,13 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe : Recipe;
+  @Output() selectedRecipe = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectRecipe(){
+    this.selectedRecipe.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -13,9 +13,16 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Second test recipe', 'this is the second test', 
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?resize=960,872?quality=90&webp=true&resize=375,341')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+
+    this.recipeWasSelected.emit(recipe);
+  }
 }
