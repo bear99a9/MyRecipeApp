b63c4c00687caa1bfecf8ceafc718d9a4b980364
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-16 18:43:00 +0100

    Added active links

--- src/app/header/app-header.component.ts
+++ src/app/header/app-header.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AppHeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
